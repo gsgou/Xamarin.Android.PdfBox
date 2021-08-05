88ecb87ed840eb21afefc627dea36d21692d48df
Giorgos Sgouridis <giorgos@live.de>
2021-08-05 15:32:41 +0200

    add bash nuget build script
    run it from proj path as ./build.sh after setting VersionPrefix in csproj or as ./build.sh major.minor.patch[.build]. Build number is omitted if not different then zero.

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#set -x
+
+DIRNAME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+PROJLOC='/Xamarin.Android.PdfBox/Xamarin.Android.PdfBox.csproj'
+CSPROJ="${DIRNAME}${PROJLOC}"
+echo "${CSPROJ}"
+
+if [ -z "$1" ]
+  then
+    msbuild $CSPROJ /t:Pack /restore /p:Configuration=Release /p:PackageOutputPath=$DIRNAME
+  else
+    msbuild $CSPROJ /t:Pack /restore /p:Configuration=Release /p:PackageOutputPath=$DIRNAME /p:PackageVersion=$1
+fi
+
+#set +x
